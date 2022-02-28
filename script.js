8ee41ba1bba135ede8093e1fdfa18bc17849a51a
Federico Sapuppo <fedesapuppo@hotmail.com>
2022-02-27 22:48:11 -0300

    add +,-,* and / functionalities

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ display.textContent = displayValue;
 
 let button1 = document.getElementById('button1');
 button1.addEventListener('click', () => {
-	if (displayValue === '0' || displayValue === '+'){
+	if (displayValue === '0' || operation !== ''){
 		displayValue = '1';
 		display.textContent = displayValue;
 	} else {
@@ -35,7 +35,7 @@ button1.addEventListener('click', () => {
 
 let button2 = document.getElementById('button2');
 button2.addEventListener('click', () => {
-	if (displayValue === '0' || displayValue === '+'){
+	if (displayValue === '0' || operation !== ''){
 		displayValue = '2';
 		display.textContent = displayValue;
 	} else {
@@ -46,7 +46,7 @@ button2.addEventListener('click', () => {
 
 let button3 = document.getElementById('button3');
 button3.addEventListener('click', () => {
-	if (displayValue === '0' || displayValue === '+'){
+	if (displayValue === '0' || operation !== ''){
 		displayValue = '3';
 		display.textContent = displayValue;
 	} else {
@@ -57,7 +57,7 @@ button3.addEventListener('click', () => {
 
 let button4 = document.getElementById('button4');
 button4.addEventListener('click', () => {
-	if (displayValue === '0'|| displayValue === '+'){
+	if (displayValue === '0' || operation !== ''){
 		displayValue = '4';
 		display.textContent = displayValue;
 	} else {
@@ -68,7 +68,7 @@ button4.addEventListener('click', () => {
 
 let button5 = document.getElementById('button5');
 button5.addEventListener('click', () => {
-	if (displayValue === '0'|| displayValue === '+'){
+	if (displayValue === '0' || operation !== ''){
 		displayValue = '5';
 		display.textContent = displayValue;
 	} else {
@@ -79,7 +79,7 @@ button5.addEventListener('click', () => {
 
 let button6 = document.getElementById('button6');
 button6.addEventListener('click', () => {
-	if (displayValue === '0'|| displayValue === '+'){
+	if (displayValue === '0' || operation !== ''){
 		displayValue = '6';
 		display.textContent = displayValue;
 	} else {
@@ -90,7 +90,7 @@ button6.addEventListener('click', () => {
 
 let button7 = document.getElementById('button7');
 button7.addEventListener('click', () => {
-	if (displayValue === '0'|| displayValue === '+'){
+	if (displayValue === '0' || operation !== ''){
 		displayValue = '7';
 		display.textContent = displayValue;
 	} else {
@@ -101,7 +101,7 @@ button7.addEventListener('click', () => {
 
 let button8 = document.getElementById('button8');
 button8.addEventListener('click', () => {
-	if (displayValue === '0'|| displayValue === '+'){
+	if (displayValue === '0' || operation !== ''){
 		displayValue = '8';
 		display.textContent = displayValue;
 	} else {
@@ -112,7 +112,7 @@ button8.addEventListener('click', () => {
 
 let button9 = document.getElementById('button9');
 button9.addEventListener('click', () => {
-	if (displayValue === '0'|| displayValue === '+'){
+	if (displayValue === '0' || operation !== ''){
 		displayValue = '9';
 		display.textContent = displayValue;
 	} else {
@@ -123,7 +123,7 @@ button9.addEventListener('click', () => {
 
 let button0 = document.getElementById('button0');
 button0.addEventListener('click', () => {
-	if (displayValue === '0'|| displayValue === '+'){
+	if (displayValue === '0' || operation !== ''){
 		displayValue = '0';
 		display.textContent = displayValue;
 	} else {
@@ -132,20 +132,59 @@ button0.addEventListener('click', () => {
 	}
 });
 
+
 let num1;
-let operation;
+let num2;
+let operation = '';
 
 let buttonEquals = document.getElementById('buttonEquals');
 buttonEquals.addEventListener('click', () => {
 	num2 = displayValue;
-	displayValue = operation(parseInt(num1) , parseInt(num2) );
+	displayValue = operation(parseFloat(num1), parseFloat(num2) );
 	display.textContent = displayValue;
+	num1 = 0;
 });
 
 let buttonPlus = document.getElementById('buttonPlus');
 buttonPlus.addEventListener('click', () => {
+	if (operation !== ''){
+	num2 = displayValue;
+	displayValue = operation(parseFloat(num1), parseFloat(num2) );
+	display.textContent = displayValue;
+	}
 	num1 = displayValue;
 	operation = add;
-	displayValue = '+';
-	display.textContent = displayValue;
+});
+
+let buttonMinus = document.getElementById('buttonMinus');
+buttonMinus.addEventListener('click', () => {
+	if (operation !== ''){
+		num2 = displayValue;
+		displayValue = operation(parseFloat(num1), parseFloat(num2) );
+		display.textContent = displayValue;
+		}
+		num1 = displayValue;
+		operation = subtract;
+});
+
+let buttonMultiply = document.getElementById('buttonMultiply');
+buttonMultiply.addEventListener('click', () => {
+	if (operation !== ''){
+		num2 = displayValue;
+		displayValue = operation(parseFloat(num1), parseFloat(num2) );
+		display.textContent = displayValue;
+		}
+		num1 = displayValue;
+		operation = multiply;
+});
+
+let buttonDivide = document.getElementById('buttonDivide');
+buttonDivide.addEventListener('click', () => {
+	if (operation !== ''){
+		num2 = displayValue;
+		displayValue = operation(parseFloat(num1), parseFloat(num2) );
+		display.textContent = displayValue;
+		}
+		num1 = displayValue;
+		operation = divide;
 });
